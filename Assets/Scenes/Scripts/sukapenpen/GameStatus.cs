771b3ae0cfac778d2c09315cb62de92f5a03b4a7
sukapenpen <ne280194@senshu-u.jp>
2019-09-12 15:25:17 +0900

    移植作業

---
+++ Assets/Scenes/Scripts/sukapenpen/GameStatus.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameStatus
+{
+   public float elapsedTime { get; protected set; }
+   
+   public abstract void Reset();
+   
+   public abstract GameStatus Run(float _time);
+}
---
+++ Assets/Scenes/Scripts/sukapenpen/SceneSelect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelect : SingletonMonoBehaviour<SceneSelect>
+{
+    private void Awake()
+    {
+        SceneManager.LoadScene("TitleScene", LoadSceneMode.Additive);
+    }
+
+    public void ChangeStageSelect()
+    {
+        SceneManager.LoadScene("StageSelectScene", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("TitleScene");
+    }
+
+    public void ChangeStage1Scene()
+    {
+        SceneManager.LoadScene("Stage1Scene", LoadSceneMode.Additive);
+        SceneManager.UnloadSceneAsync("StageSelectScene");
+    }
+}
