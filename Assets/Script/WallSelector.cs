4ac469e1cdaa5f306c1d0a1b699d00199ff59faf
mtsgi <oq@live.jp>
2019-09-13 12:07:20 +0900

    かべをえらぶやつ(仮)

---
+++ Assets/Script/WallSelector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSelector : MonoBehaviour
+{
+    bool isSelecting = false;
+    Collider selecting;
+
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider hit) {
+        if (hit.CompareTag("Wall") && !isSelecting) {
+            if (selecting) {
+                selecting.gameObject.GetComponent<Renderer>().material.color = Color.white;
+            }
+            isSelecting = true;
+            hit.gameObject.GetComponent<Renderer>().material.color = Color.blue;
+            selecting = hit;
+        }
+    }
+    void OnTriggerExit(Collider hit) {
+        if (hit == selecting && hit.CompareTag("Wall")) {
+            isSelecting = false;
+            selecting = null;
+            hit.gameObject.GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
