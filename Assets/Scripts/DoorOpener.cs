db78010437f9f6bb6a1788c209ede45ff545bbba
mtsgi <oq@live.jp>
2019-09-14 01:15:49 +0900

    オブジェクトをインポート／ミニマップの何か

---
+++ Assets/Scripts/DoorOpener.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOpener : MonoBehaviour {
+
+    string opposite = "";
+
+    void OnTriggerEnter(Collider hit) {
+        if (hit.CompareTag("DoorWall")) {
+            hit.gameObject.SetActive(false);
+            if (hit.name == "N") {
+                opposite = "S";
+            }
+            else if (hit.name == "S") {
+                opposite = "N";
+            }
+            else if (hit.name == "E") {
+                opposite = "W";
+            }
+            else if (hit.name == "W") {
+                opposite = "E";
+            }
+            hit.transform.parent.transform.Find(opposite).gameObject.SetActive(false);
+        }
+    }
+}
